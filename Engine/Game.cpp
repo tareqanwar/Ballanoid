210b8b5e86511affa0a0ef5423787fbf1514f810
Tareq Anwar <trqnwr@hotmail.com>
2018-10-03 04:15:30 -0500

    fixed issue with side bounch issue

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -53,15 +53,19 @@ Game::Game(MainWindow& wnd)
 void Game::Go()
 {
 	gfx.BeginFrame();
-	UpdateModel();
+	float elapsedTime = ft.Mark();
+	while (elapsedTime > 0.0f)
+	{
+		const float dt = std::min(0.0025f, elapsedTime);
+		UpdateModel(dt);
+		elapsedTime -= dt;
+	}
 	ComposeFrame();
 	gfx.EndFrame();
 }
 
-void Game::UpdateModel( )
+void Game::UpdateModel( float dt )
 {
-	const float dt = ft.Mark();
-
 	paddle.Update(wnd.kbd, dt);
 	paddle.DoWallCollision(walls);
 
