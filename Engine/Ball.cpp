4ba3995164f9ff563c9f62d76d099080058f9b2c
Tareq Anwar <trqnwr@hotmail.com>
2018-10-02 23:35:43 -0500

    added ball bounch from walls

--- Engine/Ball.cpp
+++ Engine/Ball.cpp
@@ -17,3 +17,47 @@ void Ball::Update(float dt)
 {
 	position += velocity * dt;
 }
+
+bool Ball::DoWallCollision(const RectF& walls)
+{
+	bool isColided = false;
+	const RectF rect = GetRect();
+
+	if (rect.left < walls.left) {
+		position.x += walls.left - rect.left;
+		ReboundX();
+		isColided = true;
+	}
+	else if (rect.right > walls.right) {
+		position.x -= rect.right - walls.right;
+		ReboundX();
+		isColided = true;
+	}
+
+	if (rect.top < walls.top) {
+		position.y += walls.top - rect.top;
+		ReboundY();
+		isColided = true;
+	}
+	else if (rect.bottom > walls.bottom) {
+		position.y -= rect.bottom - walls.bottom;
+		ReboundY();
+		isColided = true;
+	}
+	return isColided;
+}
+
+void Ball::ReboundX()
+{
+	velocity.x = -velocity.x;
+}
+
+void Ball::ReboundY()
+{
+	velocity.y = -velocity.y;
+}
+
+RectF Ball::GetRect() const
+{
+	return RectF::FormCenter(position, radius, radius);
+}
--- Engine/Ball.h
+++ Engine/Ball.h
@@ -8,6 +8,10 @@ public:
 	Ball(const Vec2& in_position, const Vec2& in_velocity);
 	void Draw(Graphics& gfx) const;
 	void Update(float dt);
+	bool DoWallCollision(const RectF& walls);
+	void ReboundX();
+	void ReboundY();
+	RectF GetRect() const;
 private:
 	static constexpr float radius = 7.0f;
 	Vec2 position;
